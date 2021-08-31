cd3d6477766e68461cc02edfcda19bee74664813
jboucly <julien.boucly@outlook.com>
2021-08-31 12:30:35 +0200

    create first menu

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,65 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, Menu, MenuItem } from "electron";
 
-app.on('ready', () => {
-    const mainWindow = new BrowserWindow({
-        width: 1280,
-        height: 720,
-        webPreferences: {
-            nodeIntegration: true,
-            webviewTag: true,
-            contextIsolation: false,
-        }
-    });
+export class Main {
+    public mainWindow: BrowserWindow;
 
-    mainWindow.loadFile('index.html');
-    mainWindow.on('ready-to-show',  () => {
-        mainWindow.show();
-        mainWindow.focus();
-    });
-});
+    constructor() {
+        this.createMainWindows();
+        this.setMenu();
+    }
+
+    public createMainWindows(): void {
+        app.on("ready", () => {
+            this.mainWindow = new BrowserWindow({
+                width: 1280,
+                height: 720,
+                webPreferences: {
+                    nodeIntegration: true,
+                    webviewTag: true,
+                    contextIsolation: false,
+                },
+            });
+
+            this.mainWindow.loadFile("index.html");
+            this.mainWindow.on("ready-to-show", () => {
+                this.mainWindow.show();
+                this.mainWindow.focus();
+            });
+        });
+    }
+
+    public setMenu(): void {
+        const menu = Menu.buildFromTemplate([
+            new MenuItem({
+                label: "Dev Tools",
+                submenu: [
+                    {
+                        label: "Show console",
+                        role: "toggleDevTools",
+                        accelerator:
+                            process.platform === "darwin"
+                                ? "Cmd+Shift+D"
+                                : "ctrl+Shift+D",
+                    },
+                    {
+                        label: "Reload app",
+                        role: "reload",
+                        accelerator:
+                            process.platform === "darwin" ? "Cmd+r" : "ctrl+r",
+                    },
+                    { type: "separator" },
+                    {
+                        label: "Exit",
+                        click: () => app.quit(),
+                        accelerator:
+                            process.platform === "darwin" ? "Cmd+q" : "ctrl+q",
+                    },
+                ],
+            }),
+        ]);
+
+        Menu.setApplicationMenu(menu);
+    }
+}
+
+new Main();
